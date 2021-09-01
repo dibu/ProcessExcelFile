6374ff84aa9a43bf4a8809445e0c584f119d1932
Dibyajyoti Das <dibu.aspnet@hotmail.com>
2021-09-01 23:21:45 +0530

    Add project files.

---
+++ ProcessExcelFile/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProcessExcelFile._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="btnProcessExcel" runat="server" Text="Procexx Excel" OnClick="btnProcessExcel_Click" />
+</asp:Content>
---
+++ ProcessExcelFile/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClosedXML;
+using ClosedXML.Excel;
+using System.IO;
+namespace ProcessExcelFile {
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void btnProcessExcel_Click(object sender, EventArgs e) {
+            try {
+                string filePath = Server.MapPath("~/ExcelFiles/MenuData.xlsx");
+                if (File.Exists(filePath)) {
+                    using (var excelWorkBook = new XLWorkbook(filePath)) {
+                        var workSheet = excelWorkBook.Worksheet("MenuList");
+                        for (int row = 2; row <= 22; row++) {
+                            //for (int column = 1; column <= 6; column++) {
+                            //    var currentCell = workSheet.Cell(row, column);
+                            //}
+
+                            var depthCell = workSheet.Cell(row, 6);
+
+                            var firstCellValue = workSheet.Cell(row, 1).GetValue<string>();
+                            var secondCellValue = workSheet.Cell(row, 2).GetValue<string>();
+                            var thirdCellValue = workSheet.Cell(row, 3).GetValue<string>();
+                            var fourthCellValue = workSheet.Cell(row, 4).GetValue<string>();
+                            if (!string.IsNullOrEmpty(fourthCellValue)) {
+                                depthCell.SetValue<int>(4).Style.Fill.BackgroundColor= XLColor.LightGreen;
+                            } else if (!string.IsNullOrEmpty(thirdCellValue) && string.IsNullOrEmpty(fourthCellValue)) {
+                                depthCell.SetValue<int>(3).Style.Fill.BackgroundColor= XLColor.Amber;
+                                
+                            } else if (!string.IsNullOrEmpty(secondCellValue) && string.IsNullOrEmpty(thirdCellValue) && string.IsNullOrEmpty(fourthCellValue)) {
+                                depthCell.SetValue<int>(2).Style.Fill.BackgroundColor = XLColor.Orange;
+                            } else {
+                                depthCell.SetValue<int>(1).Style.Fill.BackgroundColor = XLColor.Red;
+                            }
+                        }
+                        excelWorkBook.Save();
+                    }
+                }
+            } catch (Exception exp) { 
+                throw exp; 
+            }
+        }
+    }
+}
